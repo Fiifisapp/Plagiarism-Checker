90d7863a1f332dce457b36890d2e4f7935eb3f14
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2022-05-26 09:41:25 +0000

    importing libraries

--- client/src/components/PDFUpload/PDFUpload.js
+++ client/src/components/PDFUpload/PDFUpload.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import * as yup from "yup";
+
+
 
 const PDFUpload = () => {
   return (
